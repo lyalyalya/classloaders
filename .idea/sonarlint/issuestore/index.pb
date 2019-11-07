2ab93a4fcf765fe70dc2e5c5734fdbf50d81a2e5
olga.dmitrova <olga.dmitrova@rtlabs.ru>
2019-11-07 15:01:53 +0300

    init

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+J
+src/main/java/com/App.java,3\b\3b1b30c0bb8576bbc97e620980abf54486b85485
---
+++ src/main/java/com/App.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello world!");
+    }
+}
